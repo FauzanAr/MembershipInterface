4413f05ad3bc419c03aac32e26f1db02a3a29419
FauzanAr <faurinta12345@gmail.com>
2020-02-26 07:59:48 +0800

    creating new component in the app

--- src/navbar/Navigation.js
+++ src/navbar/Navigation.js
@@ -6,20 +6,23 @@ import React from 'react';
 class Navigation extends React.Component{
     render(){
         return (
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="dark" variant="dark" expand="lg">
             <Navbar.Brand href="#home">Membership</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                <Nav.Link href="#home">Promotions</Nav.Link>
-                <Nav.Link href="#link">Rewards</Nav.Link>
-                <NavDropdown title="Login" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Edit Profile</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Edit Password</NavDropdown.Item>
-                    <NavDropdown.Divider />
+                <Nav.Link href="#promotions">Promotions</Nav.Link>
+                <Nav.Link href="#rewards">Rewards</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            <Navbar.Collapse className="justify-content-end">
+            <NavDropdown title="Login" id="basic-nav-dropdown" variant="dark">
+                <NavDropdown.Item href="#action/3.1">Edit Profile</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Edit Password</NavDropdown.Item>
+                <NavDropdown.Divider />
                     <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
                 </NavDropdown>
-                </Nav>
+                <Navbar.Text><br></br></Navbar.Text>
             </Navbar.Collapse>
             </Navbar>
         )
