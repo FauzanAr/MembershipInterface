f09eef872c56225147d28c0827cb7421ed87e194
FauzanAr <faurinta12345@gmail.com>
2020-02-26 11:15:55 +0800

    Fixing navigation item

--- src/navbar/Navigation.js
+++ src/navbar/Navigation.js
@@ -10,22 +10,21 @@ class Navigation extends React.Component{
     render(){
         return (
             <Navbar bg="light" expand="lg">
-            <Navbar.Brand><Link to ='/'>Membership</Link></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Brand as={Link} to ='/'>Membership</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-na">
                 <Nav className="mr-auto">
-                <Nav.Link><Link to='/promotions'>Promotions</Link></Nav.Link>
-                <Nav.Link><Link to='/rewards'>Rewards</Link></Nav.Link>
+                <Nav.Link as={Link} to="/promotions">Promotions</Nav.Link>
+                <Nav.Link as={Link} to="/rewards">Rewards</Nav.Link>
                 </Nav>
-            </Navbar.Collapse>
-            <Navbar.Collapse className="justify-content-end">
-            <NavDropdown title="Login" id="basic-nav-dropdown" variant="dark">
-                <NavDropdown.Item href="#action/3.1">Edit Profile</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Edit Password</NavDropdown.Item>
-                <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
-                </NavDropdown>
-                <Navbar.Text><br></br></Navbar.Text>
+                <Nav>
+                    <NavDropdown title="Login" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Edit Profile</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Edit Password</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
             </Navbar.Collapse>
             </Navbar>
         )
